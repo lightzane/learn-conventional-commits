5ae1cac947fcc3b630ba877f37f704d4949ccdcc
lightzane <bhest.pat@gmail.com>
2025-09-17 10:26:11 +0800

    feat: add auto bump version

--- scripts/release.cjs
+++ scripts/release.cjs
@@ -3,6 +3,17 @@ const { execSync } = require('node:child_process')
 
 const stdio = 'inherit'
 
+/** @type { 'manual' | 'major' | 'minor' | 'patch' | 'prerelease' | string } */
+const mode = process.argv[2] || 'manual'
+
+// Bump version
+if (mode !== 'manual') {
+  const preid = mode === 'prerelease' ? '--preid alpha' : ''
+  execSync(`npm version ${mode} ${preid} --no-git-tag-version`, {
+    stdio,
+  })
+}
+
 // Build the project
 execSync('pnpm build', { stdio })
 
